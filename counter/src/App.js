9e2bf119611c5965ee87d6598f479e89f72baa2e
Toni Galata <batscott@gmail.com>
2020-03-16 19:20:45 -0600

    initial push

---
+++ counter/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+      return(
+        <div>
+          <h1>My counter App</h1>
+          <p>we wil put our counter here</p>
+        </div>
+      )
+    }
+  }
+
+export default App;
